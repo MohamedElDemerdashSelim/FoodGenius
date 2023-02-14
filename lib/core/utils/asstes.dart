ea688c4f65e9db4ca21a5bf94bdaf1279ac63a0d
Mohamed Eldemerdash <mohamedeldemerdash6@gmail.com>
2023-02-14 19:49:32 +0200

    finish splash view

---
+++ lib/core/utils/asstes.dart
@@ -0,0 +1,4 @@
+class AssetsData {
+  static const logo = 'assets/images/food_logo.png';
+  static const testImage = 'assets/images/test_image.png';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
 import 'core/utils/app_router.dart';
