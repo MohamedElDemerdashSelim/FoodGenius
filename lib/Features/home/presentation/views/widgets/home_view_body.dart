c03951bfbcb45c0c3373f0724c5a23234d613fb3
Mohamed Eldemerdash <mohamedeldemerdash6@gmail.com>
2023-02-26 18:41:12 +0200

    search bar done

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,14 +1,45 @@
 import 'package:flutter/material.dart';
-
-import '../../../../../core/utils/asstes.dart';
+import 'package:food_me/Features/home/presentation/views/widgets/search_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
-  const HomeViewBody({super.key});
+  const HomeViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [SafeArea(child: child)],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          SizedBox(
+            height: 15,
+          ),
+          welcomemessage(),
+          SizedBox(
+            height: 20,
+          ),
+          // Search bar
+          SearchBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class welcomemessage extends StatelessWidget {
+  const welcomemessage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Find best recipes for cooking',
+      style: TextStyle(
+        color: Colors.black87,
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
     );
   }
 }
